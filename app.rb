ec36dff2839f07c710bf83829f5d77cf09ff046d
Ethan <ethan@swipely.com>
2015-10-12 13:42:25 -0400

    write query params to file

--- app.rb
+++ app.rb
@@ -1,9 +1,22 @@
 require 'sinatra'
 
-PATH_TO_FILE = File.join(File.basename(__FILE__), 'messages.txt')
+PATH_TO_FILE = File.join(File.dirname(__FILE__), 'messages.txt')
 
 get '/' do
   "hello world"
-  `cat #{params[:message]} > #{PATH_TO_FILE}`
-  "Your message was received as: #{params[:message]}"
+  response_message = []
+  response_message << "Your message was received as: #{params[:message]}"
+  response_message << "Attempting to save to #{PATH_TO_FILE}"
+  output = system("echo #{params[:message]} > #{PATH_TO_FILE}")
+  if output
+    response_message << 'Save was successful'
+  else
+    response_message << 'Save was unsuccessful'
+  end
+
+  contents = `cat #{PATH_TO_FILE}`
+
+  response_message << "File Contents: #{contents}"
+
+  response_message.join('<br/>')
 end
