3bed7ff0a493a0a5491eeeedbfe693962afe2671
Ethan <ethan@swipely.com>
2015-10-12 14:57:38 -0400

    use file class to write to file

--- app.rb
+++ app.rb
@@ -3,22 +3,28 @@ require 'sinatra'
 PATH_TO_FILE = File.expand_path(File.join(File.dirname(__FILE__), 'messages.txt'))
 
 get '/' do
-  "hello world"
-  response_message = []
-  response_message << "Your message was received as: '#{params[:message]}'"
-  response_message << "Attempting to save to #{PATH_TO_FILE}"
-  output = system("echo #{params[:message]} > #{PATH_TO_FILE}")
-  if output
-    response_message << 'Save was successful'
-  else
-    response_message << 'Save was unsuccessful'
-  end
+  @response_message = []
+  @response_message << "Your message was received as: '#{params[:message]}'"
+  @response_message << "Attempting to save to #{PATH_TO_FILE}"
 
-  contents = `cat #{PATH_TO_FILE}`
-
-  response_message << "File Contents: #{contents}"
-
-  @message = response_message.join('<br/>')
+  write_to_file(params[:message], PATH_TO_FILE)
+  @contents = read_file(PATH_TO_FILE)
 
   erb :index
+end
+
+
+def write_to_file(message, path)
+  File.open(path, 'a') do |file_object|
+    file_object.puts(message)
+  end
+end
+
+
+def read_file(path)
+  file_object = File.new(path, 'r')
+  contents = file_object.readlines
+  file_object.close
+
+  contents
 end
