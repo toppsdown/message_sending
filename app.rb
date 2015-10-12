03ea24ef5dc892f0f404e35fad98e06cf1abe124
Ethan <ethan@swipely.com>
2015-10-12 13:52:04 -0400

    add more complex template

--- app.rb
+++ app.rb
@@ -1,11 +1,11 @@
 require 'sinatra'
 
-PATH_TO_FILE = File.join(File.dirname(__FILE__), 'messages.txt')
+PATH_TO_FILE = File.expand_path(File.join(File.dirname(__FILE__), 'messages.txt'))
 
 get '/' do
   "hello world"
   response_message = []
-  response_message << "Your message was received as: #{params[:message]}"
+  response_message << "Your message was received as: '#{params[:message]}'"
   response_message << "Attempting to save to #{PATH_TO_FILE}"
   output = system("echo #{params[:message]} > #{PATH_TO_FILE}")
   if output
@@ -18,8 +18,7 @@ get '/' do
 
   response_message << "File Contents: #{contents}"
 
-  response_message.join('<br/>')
+  @message = response_message.join('<br/>')
 
-  @message = params[:message]
   erb :index
 end
